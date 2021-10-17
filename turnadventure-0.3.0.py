bc29a09b3b649edf75ac89e7b0bfe711600ec5ec
Kevin <84983282+Cernical@users.noreply.github.com>
2021-10-17 17:13:13 +0200

    Update and rename turnadventure-0.2.3.py to turnadventure-0.3.0.py
    Arreglado varios errores y añadido un tema al ganar un combate.

--- turnadventure-0.3.0.py
+++ turnadventure-0.3.0.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python
 #!/bin/bash
 
-#import multiprocessing
 import time
 import subprocess
-#from playsound import playsound
 from pygame.locals import *
 from pygame import mixer
 from random import randrange
@@ -19,19 +17,24 @@ except:
     except:
         subprocess.call("windows.bat")
 """
+
 Juego = 1
+
+mixer.init()
+mixer.music.load("intro.mp3")
+mixer.music.play(-1)
 
 #Funciones----------------------------------------------------------------------
 def Mod_combate():
 
-    mixer.init()
-    mixer.music.load("lance.wav")
-    mixer.music.play()
+    mixer.music.load("lance.mp3")
+    mixer.music.play(-1)
 
     VidaJugador = 100
     VelocidadJugador = 20
     VidaEnemigo = 100
     VelocidadEnemigo = 20
+
     Combate = 1
 
     while Combate == 1:
@@ -100,40 +103,45 @@ def Mod_combate():
 
         #¿Ha acabado la partida?
         if VidaEnemigo <= 0:
+            mixer.music.stop()
+            mixer.music.load("victory.mp3")
+            mixer.music.play(-1)
             print("¡Ha ganado el jugador!")
             print()
             input("Pulsa cualquier tecla para continuar: ")
-            Combate = 0
             mixer.music.stop()
-
+            Combate = 0
+            mixer.music.load("intro.mp3")
+            mixer.music.play(-1)
         else:
             if VidaJugador <= 0:
+                mixer.music.stop()
                 print("¡Ha ganado tu enemigo!")
                 print()
                 input("Pulsa cualquier tecla para continuar: ")
                 Combate = 0
-                mixer.music.stop()
-
+                mixer.music.load("intro.mp3")
+                mixer.music.play(-1)
             else:
                 #Selección Movimientos Jugador----------------------------------------------
                 i = input("Selecciona movimiento (A,B y C): ")
                 if i == "A" or i == "a":
-                    randomnumero1=(randrange(0,15))
+                    randomnumero1=(randrange(0,35))
                     VidaEnemigo = VidaEnemigo - randomnumero1
                     print("")
                     print("Has ocasionado",randomnumero1,"de daño")
                     time.sleep(3)
                 else:
                     if i == "B" or i == "b":
-                        randomnumero1=(randrange(0,15))
+                        randomnumero1=(randrange(0,35))
                         VidaEnemigo = VidaEnemigo - randomnumero1
                         print("")
                         print("Has ocasionado",randomnumero1,"de daño")
                         time.sleep(3)
                     else:
                         if i == "C" or i == "c":
-                            randomnumero1=(randrange(0,15))
-                            VidaEnemigo = VidaEnemigo - 8
+                            randomnumero1=(randrange(0,35))
+                            VidaEnemigo = VidaEnemigo - randomnumero1
                             print("")
                             print("Has ocasionado",randomnumero1,"de daño")
                             time.sleep(3)
@@ -160,15 +168,13 @@ def Mod_combate():
                         print("El enemigo ha causado 8 de daño")
                         time.sleep(3)
 
+#Menú---------------------------------------------------------------------------
 while Juego == 1:
 
-    mixer.init()
-    mixer.music.load("intro.wav")
-    mixer.music.play()
-
+    clear()
     print("_______________________________________________________________________")
     print()
-    print("                  ¡Bienvenido a TurnAdventure! v0.2.3")
+    print("                  ¡Bienvenido a TurnAdventure! v0.3.0")
     print("_______________________________________________________________________")
     print()
     print("- Modo Random (1)")
@@ -177,6 +183,7 @@ while Juego == 1:
     print()
     print("-----------------------------------------------------------------------")
     print("")
+
     r = input("Seleccione modo de juego: ")
     if r == "1":
         mixer.music.stop()
@@ -186,7 +193,6 @@ while Juego == 1:
             mixer.music.stop()
             Juego = 0
         else:
-            mixer.music.stop()
             print("")
             print("No has seleccionado un modo correcto")
             time.sleep(3)
